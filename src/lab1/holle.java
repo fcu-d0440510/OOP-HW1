88a3920f2367c495148107f62b72309b63822643
ASUS <ASUS@ASUS-NB>
2016-10-07 00:24:20 +0800

    程式碼

---
+++ src/lab1/holle.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class holle {
+
+	public static void main(String[] args) {
+		String lyrics = "Let ''her'' go! Let ''her'' go! Can't hold ''her'' back anymore";
+		System.out.println(lyrics);
+
+	}
+
+}
